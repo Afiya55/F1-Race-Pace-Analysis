314ff8dbcf22310bafb3077c3a4b2bdf5d80684c
Afiya Fathima Q <afiya.fathimaq@gmail.com>
2026-05-23 21:20:26 +0530

    2023_Miami_GP_Race_Pace_Analysis.py
    Updated the plot style to dark background and adjusted the grid color for better visibility.

--- 2023_miami_gp_race_pace_analysis.py
+++ 2023_miami_gp_race_pace_analysis.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+plt.style.use("dark_background")
 
-df = pd.read_excel("matplotlib_starter.xlsx")
+
+df = pd.read_excel("matplotlib_starter.xlsx", header=1)
 
 plt.plot(df["Lap"], df["Verstappen"], label="Verstappen",marker="o",color="darkblue")
 plt.plot(df["Lap"], df["Sainz"], label="Sainz",marker="o",color="lightblue")
@@ -9,10 +11,13 @@ plt.plot(df["Lap"], df["Leclerc"], label="Leclerc",marker="o",color="red")
 
 plt.title("2023 Miami GP Race Pace")
 plt.legend()
-plt.savefig("matplotlib_starter_race_pace.png")
+plt.savefig("2023_miami_gp_race_pace.png")
 plt.xlabel("Lap")
 
 plt.ylabel("Lap Time(s)")
 plt.grid(True)
+plt.grid(color="white", alpha=0.2)
+
 
 plt.show()
+
