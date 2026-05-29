9a5c55d85adefe4d1a8983a9f432efd1481ea11f
Afiya Fathima Q <afiya.fathimaq@gmail.com>
2026-05-30 01:10:28 +0530

    Add files via upload

---
+++ 2023_hungary_analysis.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+df = pd.read_excel("2023_verstappen_hungary.xlsx", header = 1)
+df = df[df["Lap"] != 23]
+average_lap = df["Lap Time(s)"].mean()
+fastest_lap = df["Lap Time(s)"].min()
+slowest_lap = df["Lap Time(s)"].max()
+print("🏎️ Hungary 2023 Stint Analysis- Verstappen")
+print(f"Average Lap Time: {average_lap:.3f} seconds")
+print(f"Fastest Lap Time: {fastest_lap:.3f} seconds")
+print(f"Slowest Lap Time: {slowest_lap:.3f} seconds")
+sns.regplot(x="Lap", y="Lap Time(s)", data=df, marker="o", color="darkblue")
+plt.title("Verstappen Hungary 2023 Stint Analysis\nRegression Trend")
+plt.xlabel("Lap")
+plt.ylabel("Lap Time (s)")
+plt.grid(True)
+plt.savefig("2023_hungary_verstappen_regression.png")
+input("Press Enter to view the regression plot...")
+plt.show()
